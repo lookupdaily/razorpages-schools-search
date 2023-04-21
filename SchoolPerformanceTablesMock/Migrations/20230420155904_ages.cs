4e96d92fb74fa69e5d73febd81c54bed68077a81
Liz Daly <liz7daly@gmail.com>
2023-04-21 11:56:21 +0100

    Add fake school seed data

---
+++ SchoolPerformanceTablesMock/Migrations/20230420155904_ages.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolPerformanceTablesMock.Migrations
+{
+    /// <inheritdoc />
+    public partial class ages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MinAge",
+                table: "School",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MaxAge",
+                table: "School",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinAge",
+                table: "School",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxAge",
+                table: "School",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
--- SchoolPerformanceTablesMock/Models/School.cs
+++ SchoolPerformanceTablesMock/Models/School.cs
@@ -5,8 +5,8 @@ public class School
      public int Id { get; set; }
      public string? Name { get; set; }
      public string? SchoolType { get; set; }
-     public int MinAge { get; set; }
-     public int MaxAge { get; set; }
+     public string MinAge { get; set; }
+     public string MaxAge { get; set; }
      //TODO: add ofsted rating lookup
      public string? OfstedRating { get; set; }
      public string? Website { get; set; }
